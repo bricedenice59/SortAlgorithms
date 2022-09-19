c7175e373514a3e9e3daccf8db3e751f1686c385
Brice Grenard <brice.grenard@gmail.com>
2022-09-19 16:37:57 +0800

    Add project files.

---
+++ BubbleSort.cs
@@ -0,0 +1,41 @@
+﻿namespace SortAlgorithms
+{
+    internal class BubbleSort<T> : IComparer<T>
+    {
+        public int Compare(T? x, T? y)
+        {
+            if (x == null)
+                return 0;
+            if (y == null)
+                return 0;
+
+            if (int.Parse(x.ToString()) > int.Parse(y.ToString()))
+                return 1;
+            if (int.Parse(x.ToString()) < int.Parse(y.ToString()))
+                return -1;
+
+            return 0;
+        }
+
+        public void Sort(T[] items)
+        {
+            if (items.Count() > 0 && items[0].GetType() != typeof(int))
+                throw new NotImplementedException($"type {typeof(T)} has not yet been implemented");
+
+            var comparer = Comparer<T>.Create(Compare);
+
+            for (int i = 0; i < items.Length -1; i++)
+            {
+                for (int j = 0; j < items.Length - (i+1); j++)
+                {
+                    if(comparer.Compare(items[j], items[j + 1]) >0)
+                    {
+                        var tmp = items[j + 1];
+                        items[j + 1] = items[j];
+                        items[j] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using SortAlgorithms;
+using SortAlgorithms.Utils;
+using System.Security.Cryptography;
+
+int maxArrayLength = (int)1e5;
+int[] array = new int[maxArrayLength];
+
+for (int i = 0; i < maxArrayLength; i++)
+{
+    array[i] = RandomNumberGenerator.GetInt32(1, maxArrayLength);
+}
+
+BubbleSort<int> bubbleSort = new ();
+var distinctValues = array.Distinct().ToArray();
+
+MeasureTimePerformance _measure = new ();
+_measure.Init();
+_measure.Start();
+
+bubbleSort.Sort(distinctValues);
+
+_measure.Stop();
+
+Console.WriteLine($"Bubble tree sort function completed in ${_measure.GetElapsedTime()}");
+
+Array.Sort(array);
+
+Console.ReadLine();
+
