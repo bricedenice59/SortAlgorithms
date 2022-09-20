fc38485b0f01ce8c3fc40686dd40744f26f856b9
Brice Grenard <brice.grenard@gmail.com>
2022-09-20 09:23:16 +0800

    Add quicksort algorithm

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using SortAlgorithms.Utils;
 using System.Security.Cryptography;
 
-int maxArrayLength = (int)1e5;
+int maxArrayLength = (int)1e7;
 int[] array = new int[maxArrayLength];
 
 for (int i = 0; i < maxArrayLength; i++)
@@ -11,19 +11,28 @@ for (int i = 0; i < maxArrayLength; i++)
 }
 
 BubbleSort<int> bubbleSort = new ();
+QuickSort<int> quickSort = new ();
 var distinctValues = array.Distinct().ToArray();
 
 MeasureTimePerformance _measure = new ();
+Console.WriteLine("Sorting.... with bubble sort algorithm");
+//_measure.Init();
+//_measure.Start();
+
+//bubbleSort.Sort(distinctValues);
+
+//_measure.Stop();
+
+//Console.WriteLine($"Bubble tree sort function completed in ${_measure.GetElapsedTime()}");
+
+Console.WriteLine("Sorting.... with quicksort algorithm");
 _measure.Init();
 _measure.Start();
 
-bubbleSort.Sort(distinctValues);
+quickSort.Sort(distinctValues);
+Console.WriteLine($"Quicksort function completed in ${_measure.GetElapsedTime()}");
 
 _measure.Stop();
-
-Console.WriteLine($"Bubble tree sort function completed in ${_measure.GetElapsedTime()}");
-
-Array.Sort(array);
 
 Console.ReadLine();
 
---
+++ QuickSort.cs
@@ -0,0 +1,73 @@
+﻿namespace SortAlgorithms
+{
+    internal class QuickSort<T> : IComparer<T>
+    {
+        private Comparer<T> _comparer;
+        public int Compare(T? x, T? y)
+        {
+            if (x == null)
+                return 0;
+            if (y == null)
+                return 0;
+
+            if (int.Parse(x.ToString()) > int.Parse(y.ToString()))
+                return 1;
+            if (int.Parse(x.ToString()) < int.Parse(y.ToString()))
+                return -1;
+
+            return 0;
+        }
+
+        public void Sort(T[] items)
+        {
+            if (items.Count() > 0 && items[0].GetType() != typeof(int))
+                throw new NotImplementedException($"type {typeof(T)} has not yet been implemented");
+
+            _comparer = Comparer<T>.Create(Compare);
+
+            Sort(items, 0, items.Length -1);
+        }
+
+        private T[] Sort(T[] items, int left, int right)
+        {
+            if (left < right)
+            {
+                int partition = Partition(items, left, right);
+                if(partition >1)
+                    Sort(items, left, partition);
+                if(partition+1 < right)
+                    Sort(items, partition + 1, right);
+            }
+            return items;
+        }
+
+        private int Partition(T[] items, int left, int right)
+        {
+            var pivot = items[left];
+            var i = left;
+            var j = right;
+
+            while(i<=j)
+            {
+                while (_comparer.Compare(items[i], pivot) < 0)
+                {
+                    i++;
+                }
+                while (_comparer.Compare(items[j], pivot) > 0)
+                {
+                    j--;
+                }
+                if (i >= j) break;
+                Swap(items, i, j);
+            }
+            return j;
+        }
+
+        private void Swap(T[] items, int first, int second)
+        {
+            var tmp = items[first];
+            items[first] = items[second];
+            items[second] = tmp;
+        }
+    }
+}
